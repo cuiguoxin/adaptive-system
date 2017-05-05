48717d75930d134463f6408ded53c2727111edd7
cuiguoxin <cuiguoxin2228@163.com>
2017-05-05 16:32:48 +0800

    developing

--- build/generate_grpc_proto.sh
+++ build/generate_grpc_proto.sh
@@ -1,2 +1,4 @@
-protoc -I ../tensorflow/ --grpc_out=. --plugin=protoc-gen-grpc=grpc_cpp_plugin ../proto/helloworld.proto
-protoc -I ../tensorflow/ --cpp_out=. ../proto/helloworld.proto
+GRPC_PLUGIN_PATH=`which grpc_cpp_plugin`
+echo $GRPC_PLUGIN_PATH
+protoc -I ../tensorflow/ --grpc_out=. --plugin=protoc-gen-grpc=$GRPC_PLUGIN_PATH  --proto_path=../proto/ ../proto/helloworld.proto
+protoc -I ../tensorflow/ --cpp_out=. --proto_path=../proto/  ../proto/helloworld.proto
