60789bde24c933cd71c0e9ada6acb7489fbde043
cuiguoxin <cuiguoxin2228@163.com>
2017-05-06 16:37:34 +0800

    developing

--- test/python/store_graph.py
+++ test/python/store_graph.py
@@ -6,11 +6,19 @@ with tf.Session() as sess:
     b = tf.Variable(6.0, name='b')
     c = tf.multiply(a, b, name="c")
 
+    opt = GradientDescentOptimizer(learning_rate=0.1)
+    grads_and_vars = opt.compute_gradients(c, [a, b])
+   
     init = tf.global_variables_initializer()
     print init.name
 
     print a.eval() # 5.0
     print b.eval() # 6.0
     print c.eval() # 30.0
+
+    for grad_and_var in grads_and_vars:
+        print grad_and_var[0].name
+        print grad_and_var[0].eval()
+
     
     tf.train.write_graph(sess.graph_def, 'models/', 'graph.pb', as_text=False)
