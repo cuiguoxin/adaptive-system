1d43db667ebf0ebe3d7ee452eacc6640c5ee5170
cuiguoxin <cuiguoxin2228@163.com>
2017-05-06 15:09:46 +0800

    developing

--- build/generate_helloworld_grpc_proto.sh
+++
@@ -1,4 +0,0 @@
-GRPC_PLUGIN_PATH=`which grpc_cpp_plugin`
-echo $GRPC_PLUGIN_PATH
-protoc -I ../tensorflow/ --grpc_out=../test --plugin=protoc-gen-grpc=$GRPC_PLUGIN_PATH  --proto_path=../proto/ ../proto/helloworld.proto
-protoc -I ../tensorflow/ --cpp_out=../test --proto_path=../proto/  ../proto/helloworld.proto
