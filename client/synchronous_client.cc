8ea95bb11cd1e87e93b48c6f387154aef8cf3dfd
cuiguoxin <cuiguoxin2228@163.com>
2017-05-11 08:42:42 +0800

    dev

--- client/synchronous_client.cc
+++ client/synchronous_client.cc
@@ -32,6 +32,9 @@ namespace {
 std::unique_ptr<SystemControl::Stub> stub;
 float lr = 0.0;
 int interval = 0;
+int total_iter = 0;
+GRAD_QUANT_LEVEL grad_quant_level = GRAD_QUANT_LEVEL::NONE;
+
 std::map<std::string, Names>* get_map_names() {
   static std::map<std::string, Names> map_names;
   return &map_names;
@@ -95,7 +98,7 @@ void init_everything() {
           google::protobuf::MapPair<std::string, tensorflow::TensorProto> const&
               pair) {
         tensorflow::Tensor tensor;
-	bool is_success = tensor.FromProto(pair.second);
+        bool is_success = tensor.FromProto(pair.second);
         if (!is_success) {
           std::terminate();
         }
@@ -112,10 +115,55 @@ void init_everything() {
     print_error(tf_status);
   }
 }
+// return loss and set gradient to the first parameter
+float compute_gradient_and_loss(
+    std::map<std::string, tensorflow::Tensor>& gradients) {}
+
+PartialState collect_partial_state(
+    std::map<std::string, tensorflow::Tensor> const& gradients, float loss) {}
+
+void quantize_gradient(
+    std::map<std::string, tensorflow::Tensor> const& map_gradients, float loss,
+    NamedGradientsAndLoss& named_gradients_and_loss) {}
+
+void apply_gradient(NamedGradients const& named_gradients) {}
+
+void do_training() {
+  for (int i = 0; i < total_iter; i++) {
+    std::map<std::string, tensorflow::Tensor> map_gradients;
+    float loss = compute_gradient_and_loss(map_gradients);
+    if (i % interval == 0) {
+      PartialState partial_state = collect_partial_state(map_gradients, loss);
+      ClientContext context;
+      QuantizationLevel quantization_level;
+      gprc::Status grpc_status =
+          stub->sendState(&context, partial_state, &quantization_level);
+      if (!grpc_status.ok()) {
+        print_error(grpc_status);
+      }
+      grad_quant_level = quantization_level.level();
+    }
+    NamedGradientsAndLoss named_gradients_and_loss;
+    quantize_gradient(map_gradients, loss, named_gradients_and_loss);
+    NamedGradients named_gradients;
+    ClientContext context;
+    grpc::Status grpc_status = stub->sendGradient(
+        &context, named_gradients_and_loss, &named_gradients);
+    if (!grpc_status.ok()) {
+      print_error(grpc_status);
+    }
+    // add the gradients to variables
+    apply_gradient(named_gradients);
+  }
+}
 
 void close_session() { get_session()->Close(); }
 
-void run_logic() {}
+void run_logic() {
+  init_everything();
+
+  close_session();
+}
 }
 
 int main(int argc, char* argv[]) {
--- server/synchronous_server.cpp
+++ server/synchronous_server.cpp
@@ -1,6 +1,14 @@
+#include <algorithm>
+#include <condition_variable>
+#include <exception>
+#include <fstream>
+#include <functional>
 #include <iostream>
+#include <map>
 #include <memory>
+#include <mutex>
 #include <string>
+#include <vector>
 
 #include <grpc++/grpc++.h>
 
@@ -16,29 +24,79 @@ using grpc::Status;
 
 namespace adaptive_system {
 class RPCServiceImpl final : public SystemControl::Service {
-  Status retrieveTuple(ServerContext* context,
-                       const Empty* request,
+ public:
+  RPCServiceImpl(int interval, float lr, int total_iter, int number_of_workers,
+                 GRAD_QUANT_LEVEL grad_quant_level,
+                 std::string const& tuple_local_path)
+      : SystemControl::Service(),
+        _interval(interval),
+        _lr(lr),
+        _total_iter(total_iter),
+        _number_of_workers(number_of_workers),
+        _grad_quant_level(grad_quant_level),
+        _tuple_local_path(tuple_local_path) {}
+  Status retrieveTuple(ServerContext* context, const Empty* request,
                        Tuple* reply) override {
+    fstream input(_tuple_local_path, ios::in | ios::binary);
+    if (!input) {
+      std::cout << _tuple_local_path
+                << ": File not found.  Creating a new file." << std::endl;
+    } else if (!reply->ParseFromIstream(&input)) {
+      std::cerr << "Failed to parse tuple." << std::endl;
+      std::terminate();
+    }
     return Status::OK;
   }
 
   Status sendGradient(ServerContext* context,
-                      GradientAndLoss* request,
-                      Gradient* response) override {
+                      const NamedGradientsAndLoss* request,
+                      NamedGradients* response) override {
+    const NamedGradients& named_gradients = request->named_gradients();
+    const float loss = request->loss();
+    std::map<std::string, tensorflow::Tensor> map_gradient;
+    convert_named_gradient_to_map_gradient(named_gradients, map_gradient);
+    std::unique_lock<std::mutex> lk(_mutex_gradient);
+    _vector_map_gradient.push_back(map_gradient);
+    _condition_variable_gradient.wait(lk, [this] {
+      return _vector_map_gradient.size() == _number_of_workers;
+    });
     return Status::OK;
   }
 
-  Status sendState(ServerContext* context,
-                   PartialStateAndLoss* request,
+  Status sendState(ServerContext* context, const PartialStateAndLoss* request,
                    QuantizationLevel* response) {
     return Status::OK;
   }
+
+  // private member functions
+ private:
+  void convert_named_gradient_to_map_gradient(
+      const NamedGradients& named_gradients,
+      std::map<std::string, tensorflow::Tensor>& map_gradient) {}
+
+  // private data member
+ private:
+  const int _interval;
+  const float _lr;
+  const int _total_iter;
+  const int _number_of_workers;
+  GRAD_QUANT_LEVEL _grad_quant_level = GRAD_QUANT_LEVEL::NONE;
+
+  std::mutex _mutex_gradient;
+  std::mutex _mutex_state;
+  std::condition_variable _condition_variable_gradient;
+  std::condition_variable _condition_variable_state;
+  std::vector<std::map<std::string, tensorflow::Tensor>> _vector_map_gradient;
+  std::vector<PartialStateAndLoss> _vector_partial_state_and_loss;
+  float _last_loss;
+  std::vector<float> _vector_loss;
+  std::string _tuple_local_path;
 };
 }
 
 void RunServer() {
   std::string server_address("0.0.0.0:50051");
-  RPCServiceImpl service;
+  adaptive_system::RPCServiceImpl service;
 
   ServerBuilder builder;
   // Listen on the given address without any authentication mechanism.
