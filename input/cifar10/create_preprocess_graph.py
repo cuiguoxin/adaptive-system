9b0d311d4e163e2fe6f7bae0667f1c868a22b5db
cuiguoxin <cuiguoxin2228@163.com>
2017-05-15 19:13:30 +0800

    dev

--- input/cifar10/create_preprocess_graph.py
+++ input/cifar10/create_preprocess_graph.py
@@ -7,28 +7,28 @@ import tensorflow as tf
 
 label_bytes = 1;
 image_bytes = 3072;
+with tf.Session() as sess:
+    record_bytes = tf.placeholder(tf.uint8, shape=(3073), name="raw_input")
+    label = tf.cast(tf.slice(record_bytes, [0], [label_bytes]), tf.int32, name="label")
+    depth_major = tf.reshape(tf.slice(record_bytes, [label_bytes], [image_bytes]), [3, 32, 32])
+    # Convert from [depth, height, width] to [height, width, depth].
+    uint8image = tf.transpose(depth_major, [1, 2, 0])
+    reshaped_image = tf.cast(uint8image, tf.float32)
 
-record_bytes = tf.placeholder(tf.uint8, shape=(3073), name="raw_input")
-label = tf.cast(tf.slice(record_bytes, [0], [label_bytes]), tf.int32, name="label")
-depth_major = tf.reshape(tf.slice(record_bytes, [label_bytes], [image_bytes]), [3, 32, 32])
-# Convert from [depth, height, width] to [height, width, depth].
-uint8image = tf.transpose(depth_major, [1, 2, 0])
-reshaped_image = tf.cast(uint8image, tf.float32)
+    height = 32
+    width = 32
 
-height = IMAGE_SIZE
-width = IMAGE_SIZE
+    # Image processing for evaluation.
+    # # Crop the central [height, width] of the image.
+    resized_image = tf.image.resize_image_with_crop_or_pad(reshaped_image,
+                                                            width, height)
 
-# Image processing for evaluation.
-# # Crop the central [height, width] of the image.
-resized_image = tf.image.resize_image_with_crop_or_pad(reshaped_image,
-                                                         width, height)
+    # Subtract off the mean and divide by the variance of the pixels.
+    float_image = tf.image.per_image_standardization(resized_image)
 
-# Subtract off the mean and divide by the variance of the pixels.
-float_image = tf.image.per_image_standardization(resized_image)
+    print float_image.name
+    print label.name
+    print record_bytes.name
 
-print float_image.name
-print label.name
-print record_bytes.name
-
-tf.train.write_graph(sess.graph_def, './', 'preprocess.pb', as_text=False)
+    tf.train.write_graph(sess.graph_def, './', 'preprocess.pb', as_text=False)
 
