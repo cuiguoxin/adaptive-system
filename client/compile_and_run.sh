3b0101ae502aea823a3090d63841b6433c8105fb
cuiguoxin <cuiguoxin2228@163.com>
2017-05-16 11:32:51 +0800

    dev

--- client/compile_and_run.sh
+++ client/compile_and_run.sh
@@ -1,4 +1,4 @@
 export LD_LIBRARY_PATH=/home/cgx/git_project/adaptive-system/build/
-g++ -std=c++11 -I/home/cgx/git_project/adaptive-system/ -I/home/cgx/include/ -I/home/cgx/git_project/adaptive-system/tensorflow/ -I/home/cgx/git_project/adaptive-system/eigen-eigen  synchronous_client.cc ../proto/*.cc -o client -L/home/cgx/git_project/adaptive-system/build/ -ltensorflow_cc -lalgorithms -L/usr/local/lib -lgrpc++ -lgrpc  -lgrpc++_reflection -L/home/cgx/lib/ -lprotobuf -lpthread -ldl
-chmod a+x client
-./client
+g++ -std=c++11 -I/home/cgx/git_project/adaptive-system/ -I/home/cgx/include/ -I/home/cgx/git_project/adaptive-system/tensorflow/ -I/home/cgx/git_project/adaptive-system/eigen-eigen  synchronous_client.cc ../proto/*.cc -o client.bin -L/home/cgx/git_project/adaptive-system/build/ -ltensorflow_cc -lalgorithms -L/usr/local/lib -lgrpc++ -lgrpc  -lgrpc++_reflection -L/home/cgx/lib/ -lprotobuf -lpthread -ldl
+chmod a+x client.bin
+./client.bin
