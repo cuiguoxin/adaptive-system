a58304195e30d28ebf2007f06080a8e91191c771
cuiguoxin <cuiguoxin2228@163.com>
2017-05-16 18:21:32 +0800

    dev

---
+++ client/run.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH=/home/cgx/git_project/adaptive-system/build/
+chmod a+x client.bin
+./client.bin 10.61.1.120:50051
---
+++ server/run.sh
@@ -0,0 +1,2 @@
+export LD_LIBRARY_PATH=/home/cgx/git_project/adaptive-system/build/
+./server.bin
