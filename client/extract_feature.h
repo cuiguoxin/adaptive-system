991fb03449a835992db184426a71c9ce7a8cd219
cuiguoxin <cuiguoxin2228@163.com>
2017-05-19 16:28:29 +0800

    dev

---
+++ client/extract_feature.h
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <exception>
+#include <functional>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <memory>
+#include <string>
+#include <thread>
+#include <utility>
+#include <vector>
+
+#include <grpc++/grpc++.h>
+
+#include "proto/rpc_service.grpc.pb.h"
+
+#include "tensorflow/core/framework/graph.pb.h"
+#include "tensorflow/core/framework/tensor.h"
+#include "tensorflow/core/graph/default_device.h"
+#include "tensorflow/core/graph/graph_def_builder.h"
+#include "tensorflow/core/lib/core/threadpool.h"
+#include "tensorflow/core/lib/strings/stringprintf.h"
+#include "tensorflow/core/platform/init_main.h"
+#include "tensorflow/core/platform/logging.h"
+#include "tensorflow/core/platform/types.h"
+#include "tensorflow/core/public/session.h"
+
+namespace adaptive_system {
+tensorflow::Tensor get_feature(tensorflow::Tensor const& tensor);
+}
