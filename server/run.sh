15450e2cf70c98e8df994e501a7f1b2627c11291
cuiguoxin <cuiguoxin2228@163.com>
2017-05-19 09:05:45 +0800

    dev

--- server/run.sh
+++ server/run.sh
@@ -1,3 +1,3 @@
 export LD_LIBRARY_PATH=/home/cgx/git_project/adaptive-system/build/
 #format: interval, learning_rate, total_iter, number_wokers, level, tuple_path
-./server.bin 3, 0.1, 3000, 2, 8, /home/cgx/git_project/adaptive-system/input/cifar10/tuple_adam.pb
+./server.bin 3 0.1 3000 2 8 /home/cgx/git_project/adaptive-system/input/cifar10/tuple_adam.pb
