35a3327ea8e0a939fee7cdc64bcd8bce898f62b9
cuiguoxin <cuiguoxin2228@163.com>
2017-05-19 08:59:31 +0800

    dev

--- client/compile.sh
+++ client/compile.sh
@@ -1,4 +1,3 @@
 export LD_LIBRARY_PATH=/home/cgx/git_project/adaptive-system/build/
 g++ -std=c++11 -I/home/cgx/git_project/adaptive-system/ -I/home/cgx/include/ -I/home/cgx/git_project/adaptive-system/tensorflow/ -I/home/cgx/git_project/adaptive-system/eigen-eigen  synchronous_client.cc ../proto/*.cc ../input/cifar10/input.cc -o client.bin -L/home/cgx/git_project/adaptive-system/build/ -ltensorflow_cc -lalgorithms -L/usr/local/lib -lgrpc++ -lgrpc  -lgrpc++_reflection -L/home/cgx/lib/ -lprotobuf -lpthread -ldl
 chmod a+x client.bin
-./client.bin 10.61.1.120:50051 /home/cgx/git_project/adaptive-system/resources/cifar-10-batches-bin/data_batch_1.bin /home/cgx/git_project/adaptive-system/input/cifar10/preprocess.pb
--- client/run.sh
+++ client/run.sh
@@ -1,3 +1,3 @@
 export LD_LIBRARY_PATH=/home/cgx/git_project/adaptive-system/build/
 chmod a+x client.bin
-./client.bin 10.61.1.120:50051
+./client.bin 10.61.1.120:50051 /home/cgx/git_project/adaptive-system/resources/cifar-10-batches-bin/data_batch_1.bin /home/cgx/git_project/adaptive-system/input/cifar10/preprocess.pb
