4a1f704f51f3ddfc45e92c0b3179ff7cc2756646
cuiguoxin <cuiguoxin2228@163.com>
2017-06-14 11:17:51 +0800

    word2vec

---
+++ input/word2vec/input.h
@@ -0,0 +1,20 @@
+#ifndef WORD2VEC_INPUT_H
+#define WORD2VEC_INPUT_H
+
+#include "tensorflow/core/framework/graph.pb.h"
+#include "tensorflow/core/framework/tensor.h"
+#include "tensorflow/core/graph/default_device.h"
+#include "tensorflow/core/graph/graph_def_builder.h"
+#include "tensorflow/core/lib/core/threadpool.h"
+#include "tensorflow/core/lib/strings/stringprintf.h"
+#include "tensorflow/core/platform/env.h"
+#include "tensorflow/core/platform/init_main.h"
+#include "tensorflow/core/platform/logging.h"
+#include "tensorflow/core/platform/types.h"
+#include "tensorflow/core/public/session.h"
+namespace adaptive_system {
+	void init();
+	std::pair<tensorflow::Tensor, tensorflow::Tensor> get_next_batch();
+}
+
+#endif // !WORD2VEC_INPUT_H
--- proto/generate_rpc_service_proto.sh
+++ proto/generate_rpc_service_proto.sh
@@ -1,4 +1,4 @@
 GRPC_PLUGIN_PATH=`which grpc_cpp_plugin`
 echo $GRPC_PLUGIN_PATH
 protoc -I /home/cgx/git_project/adaptive-system/tensorflow/ --grpc_out=/home/cgx/git_project/adaptive-system/proto/ --plugin=protoc-gen-grpc=$GRPC_PLUGIN_PATH  --proto_path=/home/cgx/git_project/adaptive-system/proto/ /home/cgx/git_project/adaptive-system/proto/rpc_service.proto
-protoc -I /home/cgx/git_project/adaptive-system/tensorflow/ --cpp_out=/home/cgx/git_project/adaptive-system/proto/ --python_out=/home/cgx/git_project/adaptive-system/input/cifar10/ --proto_path=/home/cgx/git_project/adaptive-system/proto/  /home/cgx/git_project/adaptive-system/proto/rpc_service.proto
+protoc -I /home/cgx/git_project/adaptive-system/tensorflow/ --cpp_out=/home/cgx/git_project/adaptive-system/proto/ --python_out=/home/cgx/git_project/adaptive-system/input/word2vec/ --proto_path=/home/cgx/git_project/adaptive-system/proto/  /home/cgx/git_project/adaptive-system/proto/rpc_service.proto
