2e649cb9766a25934ea901c3b5596c5723905068
cuiguoxin <cuiguoxin2228@163.com>
2017-11-06 17:34:54 +0800

    change

---
+++ single_implement/compile.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH=/home/cgx/git_project/adaptive-system/build/
+g++ -std=c++11 -I/home/cgx/git_project/adaptive-system/ -I/home/cgx/include/ -I/home/cgx/git_project/adaptive-system/tensorflow/ -I/home/cgx/git_project/adaptive-system/eigen-eigen implement.cpp ../proto/*.cc -o single_implement.bin -L/home/cgx/git_project/adaptive-system/build/ -ltensorflow_cc -lalgorithms -L/usr/local/lib -lgrpc++ -lgrpc  -lgrpc++_reflection -L/home/cgx/lib/ -lprotobuf -lpthread -ldl
+chmod a+x single_implement.bin
---
+++ single_implement/run.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH=/home/cgx/git_project/adaptive-system/build/:/home/cgx/grpc/libs/opt/:/home/cgx/lib
+#                 iter_num, worker_num, init_level, interval
+./single_implement.bin 3000 6 2 4
