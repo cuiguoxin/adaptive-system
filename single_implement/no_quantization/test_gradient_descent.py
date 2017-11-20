b6a58e398531aae803c63cfdfc295b7999c574e2
guoxin cui <cuiguoxin2228@163.com>
2017-11-20 14:57:58 +0800

    change

--- single_implement/no_quantization/test_gradient_descent.py
+++ single_implement/no_quantization/test_gradient_descent.py
@@ -106,14 +106,14 @@ def inference(images, tup):
   pool2 = tf.nn.max_pool(norm2, ksize=[1, 3, 3, 1],
                          strides=[1, 2, 2, 1], padding='SAME', name='pool2')
 
-  factor = 8
+  factor = 10
   # local3, 7*7*64*384*3 = 3.612672M 3.612672M*4 = 14.450688MB
   with tf.variable_scope('local3') as scope:
     # Move everything into depth so we can perform a single matrix multiply.
     reshape = tf.reshape(pool2, [batch_size, -1])
     dim = reshape.get_shape()[1].value
     weights = _variable_with_weight_decay('weights', shape=[dim, 384*factor],
-                                          stddev=0.03, wd=0.001, tup=tup)
+                                          stddev=0.01, wd=0.001, tup=tup)
     biases = _variable_on_cpu('biases', [384*factor], tf.constant_initializer(0.1), tup)
     local3 = tf.nn.relu(tf.matmul(reshape, weights) + biases, name=scope.name)
 
@@ -121,7 +121,7 @@ def inference(images, tup):
   # local4, 384*3*1920 = 2.211840M, 2.211840*4 = 8.847360MB
   with tf.variable_scope('local4') as scope:
     weights = _variable_with_weight_decay('weights', shape=[384*factor, dim2],
-                                          stddev=0.03, wd=0.001, tup=tup)
+                                          stddev=0.01, wd=0.001, tup=tup)
     biases = _variable_on_cpu('biases', [dim2], tf.constant_initializer(0.1), tup)
     local4 = tf.nn.relu(tf.matmul(local3, weights) + biases, name=scope.name)
 
