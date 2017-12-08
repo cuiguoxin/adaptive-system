09fda134f806db3df9b6790d25121092a99968d2
guoxin cui <cuiguoxin2228@163.com>
2017-12-08 10:29:39 +0800

    change

--- single_implement/run_sarsa.sh
+++ single_implement/run_sarsa.sh
@@ -1,3 +1,3 @@
 export LD_LIBRARY_PATH=/home/cgx/git_project/adaptive-system/build/:/home/cgx/grpc/libs/opt/:/home/cgx/lib
 #                 iter_num, worker_num, init_level, interval
-./single_implement_sarsa.bin 5000 12 2 5 2 8 0.1
+./single_implement_sarsa.bin 5000 12 2 5 2 8 0.1 0.7 0.1
