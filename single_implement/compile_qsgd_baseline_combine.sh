b8ffae281ca1da0e1ed5381ce67c1c0722b667e1
guoxin cui <cuiguoxin2228@163.com>
2018-01-02 15:31:21 +0800

    change

---
+++ single_implement/compile_qsgd_baseline_combine.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH=/home/cgx/git_project/adaptive-system/build/
+g++ -std=c++11 -I/home/cgx/git_project/adaptive-system/ -I/home/cgx/include/ -I/home/cgx/git_project/adaptive-system/tensorflow/ -I/home/cgx/git_project/adaptive-system/eigen-eigen implement.cpp ../proto/*.cc accuracy.cc -o single_implement_qsgd_baseline_combine.bin -L/home/cgx/git_project/adaptive-system/build/ -ltensorflow_cc -lalgorithms -L/usr/local/lib -lgrpc++ -lgrpc  -lgrpc++_reflection -L/home/cgx/lib/ -lprotobuf -lpthread -ldl
+chmod a+x single_implement_qsgd_baseline_combine.bin
