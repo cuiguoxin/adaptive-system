4cbef16e166a022e8f709e21f745d04ef98546dc
guoxin cui <cuiguoxin2228@163.com>
2018-01-02 15:28:43 +0800

    change

--- single_implement/run_qsgd_sarsa.sh
+++ single_implement/run_qsgd_sarsa.sh
@@ -1,3 +1,3 @@
 export LD_LIBRARY_PATH=/home/cgx/git_project/adaptive-system/build/:/home/cgx/grpc/libs/opt/:/home/cgx/lib
 #                 iter_num, worker_num, init_level, interval, start_level, end_level, eps_greedy, r, learning_rate, start_iter, predict_interval
-./single_implement_sarsa.bin 4000 18 2 5 2 8 0.1 0.8 0.15 20 5.0 7.5 10
+./single_implement_qsgd_sarsa.bin 4000 18 2 5 2 8 0.1 0.8 0.15 20 5.0 7.5 10
