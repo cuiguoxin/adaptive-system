b14d54bba31927cb61967537d6d8a046e8d87e5c
guoxin cui <cuiguoxin2228@163.com>
2018-01-04 19:49:33 +0800

    change

---
+++ single_implement/compile_adaptive_according_norm_baseline_combine.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH=/home/cgx/git_project/adaptive-system/build/
+g++ -std=c++11 -I/home/cgx/git_project/adaptive-system/ -I/home/cgx/include/ -I/home/cgx/git_project/adaptive-system/tensorflow/ -I/home/cgx/git_project/adaptive-system/eigen-eigen implement.cpp ../proto/*.cc accuracy.cc ../quantization/util/*.cc -o single_implement_adaptive_according_norm_baseline_combine.bin -L/home/cgx/git_project/adaptive-system/build/ -ltensorflow_cc -L/usr/local/lib -lgrpc++ -lgrpc  -lgrpc++_reflection -L/home/cgx/lib/ -lprotobuf -lpthread -ldl
+chmod a+x single_implement_adaptive_according_norm_baseline_combine.bin
--- single_implement/run_adaptive_according_norm_baseline_combine.sh
+++ single_implement/run_adaptive_according_norm_baseline_combine.sh
@@ -1,4 +1,4 @@
 export LD_LIBRARY_PATH=/home/cgx/git_project/adaptive-system/build/:/home/cgx/grpc/libs/opt/:/home/cgx/lib
 # iter_num, worker_num, base_level, init_lr, start_iter_num, predict_interval
-./single_implement_baseline_combine.bin 4000 12 2 0.15 30 10
+./single_implement_adaptive_according_norm_baseline_combine.bin 4000 12 2 0.15 30 10
  
